a7ae032eeb198d0f579b183a66774e70ef0e2e05
zhangheng <wb-zh407235@antfin.com>
2019-03-22 18:44:03 +0800

    弹窗完成

--- src/IRouter.js
+++ src/IRouter.js
@@ -4,17 +4,23 @@ import App from './App';
 import Admin from './admin';
 import Buttons from './pages/admin/ui/buttons';
 import NoMatched from './pages/noMatched';
+import Modals from './pages/admin/ui/modals';
 
 export default class IRooter extends React.Component {
     render() {
         return (
             <Router>
                 <App>
-
-                    <Route path="/admin" render={()=><Admin><Route path="/admin/ui/buttons" component={Buttons}></Route></Admin>}></Route>
                     {/*<Route path="/login" component={Login}></Route>
                     <Route path="/order/detail" component={}></Route>*/}
+                    <Route path="/admin" render={() => <Admin>
+                        <Route path="/admin/ui/buttons" component={Buttons}></Route>
+                        <Route path="/admin/ui/modals" component={Modals}></Route>
+                    </Admin>}>
+                    </Route>
+
                     <Route component={NoMatched}></Route>
+
                 </App>
             </Router>
         )
--- src/pages/admin/ui/buttons/index.js
+++ src/pages/admin/ui/buttons/index.js
@@ -1,10 +1,62 @@
 import React from 'react';
+import {Card,Button,Radio} from 'antd';
+import './index.less';
 export default class Buttons extends React.Component {
+    state ={
+       loading:true,
+        size:"default"
+    }
+    handleCloseLoading=()=>{
+        console.log("handleCloseLoading")
+       this.setState({
+           loading: false,
+       })
+    }
+    handleChange=(e)=>{
+      let size =   e.target.value;
+      this.setState({
+          size
+      })
+    }
+
     render() {
         return (
             <div>
-                <p>this this Buttons page!!</p>
+                <Card title={"基础按钮"} className={"card-warp"}>
+                    <Button type={"primary"}>primary</Button>
+                    <Button type={"dashed"}>dashed</Button>
+                    <Button>default</Button>
+                    <Button type="danger">danger</Button>
+                </Card>
+                <Card title={"图形按钮"} className={"card-warp"}>
+                    <Button icon={"plus"}></Button>
+                    <Button icon={"delete"}></Button>
+                    <Button icon="search" shape="circle"></Button>
+                    <Button icon="edit"></Button>
+                    <Button icon="download">download</Button>
+
+                </Card>
+                <Card title={"加载"} className={"card-warp"}>
+                    <Button loading={this.state.loading} type={"primary"}>保存中...</Button>
+                    <Button type="primary" shape="circle" loading={this.state.loading}/>
+                    <Button onClick={this.handleCloseLoading}>关闭loading</Button>
+                </Card>
+                <Card title={"按钮组"} style={{marginBottom:10}}>
+                    <Button.Group>
+                        <Button type="primary" icon={"double-left"}>向左</Button>
+                        <Button type={"primary"} icon={"double-right"}>向右</Button>
+                    </Button.Group>
+                </Card>
+                <Card title={"按钮尺寸"}>
+                    <Radio.Group value={this.state.size} onChange={this.handleChange}>
+                        <Radio value={"small"}></Radio>
+                        <Radio value={"default"}></Radio>
+                        <Radio value={"large"}></Radio>
+                    </Radio.Group>
+                    <Button type="primary" size={this.state.size}>{this.state.size}</Button>
+                </Card>
             </div>
+
         );
     }
 }
